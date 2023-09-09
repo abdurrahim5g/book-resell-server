4e329dba5950566b68d7b549dac5ce61828e9afd
f-rahim <freelancerrahim26@gmail.com>
2023-09-09 10:58:19 +0600

    Saved user information if the user is not exist

--- router/user.js
+++ router/user.js
@@ -7,15 +7,36 @@ const run = async () => {
     const db = client.db("bookresell");
     const userCollection = db.collection("users");
 
+    const isUserExist = async (email) => {
+      const userFilter = { email };
+      const result = await userCollection.findOne(userFilter);
+      if (email === result?.email) {
+        return true;
+      }
+      return false;
+    };
+
     router.get("/", (req, res) => {
       res.send("All users");
     });
 
     router.post("/", async (req, res) => {
       const userDoc = req.body;
-      const result = await userCollection.insertOne(userDoc);
-      console.log(result);
-      res.status(200).send(result);
+      // console.log(isUserExist(userDoc?.email));
+      isUserExist(userDoc?.email)
+        .then((user) => {
+          // console.log("[User Exist]", user);
+          if (!user) {
+            const saveUserInfo = async () => {
+              const result = await userCollection.insertOne(userDoc);
+              return res.status(200).send(result);
+            };
+            saveUserInfo();
+          } else {
+            return res.send({ acknowledged: true });
+          }
+        })
+        .catch((err) => console.log(err));
     });
   } finally {
     // Finally
